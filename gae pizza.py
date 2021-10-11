c01545f2d2d724bf286aa5ffd8c241336d19b919
RD-BrickedUp <89468168+RD-BrickedUp@users.noreply.github.com>
2021-10-11 08:15:11 -0400

    Create gae pizza.py

---
+++ gae pizza.py
@@ -0,0 +1,10 @@
+toppings = ["pepperoni", "pineapple", "cheese", "sausage", "olives", "anchovies", "mushrooms"]
+prices = [2, 6, 1, 3, 2, 7, 2]
+
+num_two_dollar_slices = 3
+num_pizzas = str(7)
+print("We sell " + num_pizzas + " different kinds of pizza!")
+
+pizza_and_prices = [[2, 'pepperoni'], [6, 'pineapple'], [1, 'cheese'], [3, 'sausage'], [2, 'olives'], [7, 'anchovies'], [2, 'mushrooms']]
+pizza_and_prices.sort()
+print(pizza_and_prices)
